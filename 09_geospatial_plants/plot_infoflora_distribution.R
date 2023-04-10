4592f6561550cd76b246e4fad2f9ba9dbb40d486
Yi Wang <wangyi9122@yahoo.com>
2023-04-10 08:23:45 +0200

    Update plot_infoflora_distribution.R

--- 09_geospatial_plants/plot_infoflora_distribution.R
+++ 09_geospatial_plants/plot_infoflora_distribution.R
@@ -10,31 +10,7 @@ plot_infoflora_distribution <- function(df, tab){
   # df is a dataframe with columns "x_wgs84", "y_wgs84", "obs_nb" and "species", "genus"
   # tab is a dataframe combining reads and long/lat of a taxon
   
-  
-  # if to transfer coordination system, transfer data.frame to SpatialPointsDataFrame-class
-  #df_spd <- SpatialPoints(df[,c("x_wgs84", "y_wgs84")], proj4string=CRS("+proj=longlat +datum=WGS84"))
-  
-  # create a data frame variable with coordinates and info we want to plot".
-  #tmp = as.data.frame(df_spd)
-  #colnames(tmp) = c("long", "lat")
-  #tmp$obs_nb = df$obs_nb
-  #tmp$species = df$species
-  #tmp$genus = df$genus
-  
-  
-  
-  
-  # Use ggmap to plot coordinates on Google Maps.
-  # Set my API Key. Here you need paste your own API.
-  ggmap::register_google(key = "MY_API_KEY")
-  
-  # get map of Lake Constance and catchment area
-  #map <- get_googlemap(center = c(lon = 9.190567, lat = 46.8),
-  #                     zoom = 8, scale = 2, size = c(1280, 1280),
-  #                     maptype ='terrain',
-  #                     color = 'color',
-  #                     path = "&style=feature:all|element:labels|visibility:off") 
-  
+    
   map <- get_map(location = c(lon = 10.2, lat = 46.8),
                        zoom = 8, scale = 2, size = c(1280, 1280),
                        maptype ='terrain-background',
